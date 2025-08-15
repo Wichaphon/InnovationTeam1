6576944c7413f1cee46b4fa3977f6c4c70aefc25
Edward_Bacara <tutorship57@gmail.com>
2025-08-16 02:45:31 +0700

    feat prisma file and fix dockerfile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,14 @@
 import express = require('express');
 
 const app = express();
+const route = express.Router();
+
 
 const PORT = 5000;
+
+app.get('/kuy', (req, res) => {
+    res.status(200).send('Hello World');
+})
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`)
