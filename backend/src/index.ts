d241c63019a799788e144e332971b33b9a5bf849
Wichaphon <horokindov3310@gmail.com>
2025-09-03 17:09:01 +0700

    Implement Prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
+
+const prisma = new PrismaClient();
+
 
 const app = express();
 const PORT = process.env.PORT;
